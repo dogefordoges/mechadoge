65b7c23e3e17f006d1447bd615b58d0340b633fc
hswick <hdswick@gmail.com>
2018-07-04 19:37:39 -0400

    local scope processing working

--- src/lib.rs
+++ src/lib.rs
@@ -34,60 +34,112 @@ mod processor {
         return keeping;
     }
 
+    fn local_scope_helper(mut lines: Vec<String>, line_number: usize, mut function_count: usize) -> (Vec<String>, usize) {
+        let mut i: usize = line_number.clone();
+        let mut new_lines: Vec<String> = Vec::<String>::new();        
+        let mut args = HashMap::<String, String>::new();
+        let original_function_count = function_count;
+        let mut function_end = false;
+
+        loop {
+            
+            if i == lines.len() {
+                panic!("Missing delimiter!");
+            }
+
+            if lines[i].contains("much") {
+
+                if original_function_count == function_count {
+                    let l: String = lines[i].clone();
+
+                    let split_line: Vec<&str> = l.split("much ").collect();
+                    let split_args: Vec<&str> = split_line[1].split(" ").collect();
+
+                    let mut k = 0;
+                    for arg in split_args {
+                        let new_arg_name: String = format!("{}_{}", original_function_count.to_string(), k.to_string());
+                        args.insert(arg.to_string(), new_arg_name);
+                        k = k + 1;
+                    }
+                    
+                    function_count = function_count + 1;
+                    
+                } else {
+                    let (returned_lines, new_function_count) = local_scope_helper(lines.clone(), i, function_count);
+
+                    for j in 0..returned_lines.len() {
+                        let new_line: &str = &returned_lines[j];
+                        lines[i+j] = new_line.to_string();
+                    }
+
+                    function_count = new_function_count;
+                }
+
+                let func_start: String = format!("FUNC_{}", original_function_count.to_string());
+
+                lines[i] = lines[i].replace("much", &func_start);                
+                
+            }
+
+            if lines[i].contains("wow") {
+                lines[i] = lines[i].replace("wow", &format!("FUNC_END_{}", original_function_count.to_string()));
+
+                function_end = true;
+            }
+
+            for k in args.keys() {
+                let new_arg: String = args.get(k).unwrap().to_string();
+                if lines[i].contains(k) {
+                    lines[i] = lines[i].replace(k, &new_arg);
+                }
+            }
+
+            let new_line: &str = &lines[i];
+
+            new_lines.push(new_line.to_string());
+
+            if function_end {
+                break
+            }
+
+            i = i + 1;
+        }
+
+        return (new_lines, function_count);
+    }
+
     pub fn process_local_scope(mut lines: Vec<String>) -> Vec<String> {
         let mut i = 0;
+        let mut function_count = 0;
 
         loop {
             if i == lines.len() {
                 break
-            }           
+            }
 
             if lines[i].contains("much") {
+                let (new_lines, new_function_count) = local_scope_helper(lines.clone(), i, function_count);
 
-                let l: String = lines[i].clone();
-               
-                let split_line: Vec<&str> = l.split("much ").collect();
-                let split_args: Vec<&str> = split_line[1].split(" ").collect();
-                let args: Vec<&str> = split_args.to_vec();            
-
-                let mut j = i;
-                let mut function_start_counter = 0;
-                let mut function_end_counter = 0;
-
+                let mut j = 0;
                 loop {
+
+                    if j == new_lines.len() {
+                        break
+                    }
+
+                    let new_line: &str = &new_lines[j];
                     
-                    if j == lines.len() {
-                        panic!("missing delimiter!");
-                    }
-                    
-                    let mut l2: String = lines[j].clone();
-
-                    if l2.contains("much") {
-                        function_start_counter = function_start_counter + 1;
-                    }                
-
-                    if l2.contains("wow") {
-                        function_end_counter = function_end_counter + 1;
-                    }
-
-                    for index in 0..args.len() {
-                        if l2.contains(args[index]) {
-                            l2 = l2.replace(args[index], &index.to_string());
-                            lines[j] = l2.clone();
-                        }
-                    }
+                    lines[j] = new_line.to_string();
 
                     j = j + 1;
-
-                    if function_start_counter == function_end_counter {
-                        break
-                    }                    
- 
                 }
-            }
-            
-            i = i + 1;
-            
+
+                function_count = new_function_count;
+
+                i = i + new_lines.len();
+            } else {
+                i = i + 1;
+            }            
         }
 
         return lines;
@@ -180,7 +232,6 @@ mod processor {
                 }
 
                 let new_str: String = mechadoge_str.into_iter().collect();
-                println!("{}", new_str);
 
                 lines[i] = lines[i].replace(&new_str, &string_count.to_string());
 
@@ -194,6 +245,65 @@ mod processor {
         }
 
         return (lines, string_heap);
+    }
+
+    pub fn process_functions(mut lines: Vec<String>) -> Vec<String> {
+        let mut i = 0;
+
+        loop {
+            if i == lines.len() {
+                break
+            }           
+
+            if lines[i].contains("much") {
+
+                let l: String = lines[i].clone();
+               
+                let split_line: Vec<&str> = l.split("much ").collect();
+                let split_args: Vec<&str> = split_line[1].split(" ").collect();
+                let args: Vec<&str> = split_args.to_vec();            
+
+                let mut j = i;
+                let mut function_start_counter = 0;
+                let mut function_end_counter = 0;
+
+                loop {
+                    
+                    if j == lines.len() {
+                        panic!("missing delimiter!");
+                    }
+                    
+                    let mut l2: String = lines[j].clone();
+
+                    if l2.contains("much") {
+                        function_start_counter = function_start_counter + 1;
+                    }                
+
+                    if l2.contains("wow") {
+                        function_end_counter = function_end_counter + 1;
+                    }
+
+                    for index in 0..args.len() {
+                        if l2.contains(args[index]) {
+                            l2 = l2.replace(args[index], &index.to_string());
+                            lines[j] = l2.clone();
+                        }
+                    }
+
+                    j = j + 1;
+
+                    if function_start_counter == function_end_counter {
+                        break
+                    }                    
+ 
+                }
+            }
+            
+            i = i + 1;
+            
+        }
+
+        return lines;
     }
     
 }
@@ -262,5 +372,5 @@ mod processor_tests {
             assert_eq!(output[i], output_lines[i]);
         }
     }
-        
+
 }
